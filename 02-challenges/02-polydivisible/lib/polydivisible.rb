d6f9ad2282fc07958ee3c7a0002e9da53f42903b
iva mudronja <iva.pilizota@gmail.com>
2020-12-26 14:05:07 +0100

    polydivisible in base

--- 02-challenges/02-polydivisible/lib/polydivisible.rb
+++ 02-challenges/02-polydivisible/lib/polydivisible.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-def polydivisible?(num)
-  nums = num.digits.reverse
+def polydivisible?(num, base)
+  nums = num.to_s.chars
   i = 0
   arr = []
   while i < nums.length
     arr << nums[0..i]
     i += 1
   end
-  arr.map! { |n| n.join.to_i % (arr.index(n) + 1) }
+  arr.map! { |n| n.join.to_i(base) % (arr.index(n) + 1) }
      .all?(&:zero?)
 end
--- 02-challenges/02-polydivisible/spec/polydivisible_spec.rb
+++ 02-challenges/02-polydivisible/spec/polydivisible_spec.rb
@@ -3,12 +3,14 @@
 require 'polydivisible'
 
 describe 'polydivisible?' do
-  it 'should take 1 parameter' do
-    expect(method(:polydivisible?).arity).to eq(1)
+  it 'should take 2 parameter' do
+    expect(method(:polydivisible?).arity).to eq(2)
   end
 
   it 'should return true or false' do
-    expect(polydivisible?(1232)).to eq(true)
-    expect(polydivisible?(1233)).to eq(false)
+    expect(polydivisible?(1232, 10)).to eq(true)
+    expect(polydivisible?(1233, 10)).to eq(false)
+    expect(polydivisible?(123_220, 6)).to eq(true)
+    expect(polydivisible?(123_220, 10)).to eq(false)
   end
 end
