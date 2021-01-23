4e5f43f89db16f571a76bb29851504cd5f9dc8e2
iva mudronja <iva.pilizota@gmail.com>
2021-01-23 15:50:24 +0100

    todo

---
+++ 03-math/03-rect-overlap/lib/rect_overlap.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# TODO: given coordinates of 2 rectangles as multidimensional array
+# [[[0,0],[0,-2],[3,0],[3,-2]], [[2,-1],[3,-1],[2,3],[3,3]]
+# calculate overlap area if they overlap, if not return false
