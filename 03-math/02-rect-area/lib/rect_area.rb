89164cf1638c17e9c95d53e8ed3d4b42ea97bfaf
iva mudronja <iva.pilizota@gmail.com>
2021-01-23 18:01:08 +0100

    rectangle area

--- 03-math/02-rect-area/lib/rect_area.rb
+++ 03-math/02-rect-area/lib/rect_area.rb
@@ -4,4 +4,9 @@
 # calculate area
 
 def rect_area(arr)
+  x = arr.map { |e| e[0] }.uniq
+  y = arr.map { |e| e[1] }.uniq
+  width = x.max - x.min
+  height = y.max - y.min
+  (width * height).abs
 end
--- 03-math/02-rect-area/spec/rect_area_spec.rb
+++ 03-math/02-rect-area/spec/rect_area_spec.rb
@@ -5,5 +5,10 @@ require 'rect_area'
 describe 'rect_area' do
   it 'should calculate area' do
     expect(rect_area([[0, 0], [0, -2], [3, 0], [3, -2]])).to eq(6)
+    expect(rect_area([[2, -1], [3, -1], [2, 3], [3, 3]])).to eq(4)
+    expect(rect_area([[0, 0], [-3, 0], [0, -3], [-3, -3]])).to eq(9)
+    expect(rect_area([[0, 0], [-3, 0], [0, 3], [-3, 3]])).to eq(9)
+    expect(rect_area([[0, 0], [3, 0], [0, 3], [3, 3]])).to eq(9)
+    expect(rect_area([[-1, -1], [-3, -1], [-1, -3], [-3, -3]])).to eq(4)
   end
 end
