f379f25a2125935738a75b34486dd3bc316bd755
iva mudronja <iva.pilizota@gmail.com>
2021-01-24 19:26:39 +0100

    deleted unnecessary

--- 03-math/01-coins/lib/coins.rb
+++ 03-math/01-coins/lib/coins.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # TODO: given a positive integer ranging from 1 to 250
-# and coins [1, 9, 7, 5, 3, 1]
+# and coins [11, 9, 7, 5, 3, 1]
 # return the least number of coins which sum is the given integer.
 
 def coins_amount(num)
@@ -9,10 +9,8 @@ def coins_amount(num)
   amount = 0
   while num.positive?
     coins.each do |c|
-      if num / c >= 1
-        amount += num / c
-        num = num % c
-      end
+      amount += num / c
+      num = num % c
     end
   end
   amount
--- 03-math/01-coins/spec/coins_spec.rb
+++ 03-math/01-coins/spec/coins_spec.rb
@@ -5,5 +5,7 @@ require 'coins'
 describe 'coins_amount' do
   it 'should return a least amount of coins' do
     expect(coins_amount(25)).to eq(3)
+    expect(coins_amount(34)).to eq(4)
+    expect(coins_amount(14)).to eq(2)
   end
 end
